fcee15e5f443d7e4b537360e17986675ad8b2f81
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-11-19 23:26:45 +0530

    add tc executed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("scroll", handleScroll);
 
 function handleScroll() {
   let ele = document.getElementById("infinite");
-
+  //add new div
   let div = document.createElement("div");
   div.className = "load";
 
